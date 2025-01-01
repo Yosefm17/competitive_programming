37a75b659a46e23fd1edc9db44ca50f5c876f389
yosefm17 <yosefmulugeta23@gmail.com>
2025-01-01 08:43:04 -1000

    solve 2 problems a merge sort & roman-to-integeR

---
+++ Python/Roman-To-Integer/roman_to_integer.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Mapping of Roman numerals to their integer values
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        # Iterate through each character in the Roman numeral
+        for char in reversed(s):
+            current_value = roman_to_int[char]
+            
+            # If the current value is less than the previous value, subtract it
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                # Otherwise, add it
+                total += current_value
+            
+            # Update the previous value for the next iteration
+            prev_value = current_value
+        
+        return total
